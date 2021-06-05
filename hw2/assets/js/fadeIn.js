646c2fd8ca7142eebd5cbe43aab7c30e41befccf
Vladyslav Polushyn <polexodus@gmail.com>
2021-06-05 23:07:20 +0300

    Tips section added and styled

--- hw2/assets/js/fadeIn.js
+++ hw2/assets/js/fadeIn.js
@@ -1,26 +1,23 @@
-function onEntry(entry) {
-  entry.forEach(change => {
-    if (change.isIntersecting) {
-      change.target.classList.add('fade-show');
-    }
-  });
+// function onEntry(entry) {
+//   entry.forEach(change => {
+//     if (change.isIntersecting) {
+//       change.target.classList.add('fade-show');
+//     }
+//   });
 
-  for (let elm of elements) {
-		observer.observe(elm);
-	}
-}
-
-const options = { threshold: [0.5] };
-const observer = new IntersectionObserver(onEntry, options);
-// There are secondary displayed elements
-const elements = document.querySelectorAll('.fade-in');
-// It's the first displayed element
-const mainElement = document.querySelectorAll('.fade-in-main');
-
-for (let elm of mainElement) {
-  observer.observe(elm);
-}
-
-// for (let elm of elements) {
-// 	observer.observe(elm);
+//   for (let elm of elements) {
+// 		observer.observe(elm);
+// 	}
 // }
+
+// const options = { threshold: [0.5] };
+// const observer = new IntersectionObserver(onEntry, options);
+// // There are secondary displayed elements
+// const elements = document.querySelectorAll('.fade-in');
+// // It's the first displayed element
+// const mainElement = document.querySelectorAll('.fade-in-main');
+
+// for (let elm of mainElement) {
+//   observer.observe(elm);
+// }
+
