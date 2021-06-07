0abfb634cf2c7d265df2a5ab382e7fa591bb4d1a
Vladyslav Polushyn <polexodus@gmail.com>
2021-06-07 17:53:32 +0300

    Added plans slider

--- hw2/assets/js/slickSlider.js
+++ hw2/assets/js/slickSlider.js
@@ -1,6 +1,48 @@
 $(document).ready(function(){
+
   $('.problems__slider-container').slick({
   	arrows: false,
-  	dots: true
+  	dots: true,
+  	autoplay: true,
+  	autoplaySpeed: 2000,
   });
+
+  $('.plans__slider-container').slick({
+  	arrows: false,
+	  infinite: false,
+	  speed: 300,
+	  slidesToScroll: 1,
+	  autoplay: true,
+  	autoplaySpeed: 2000,
+	  // centerMode: true,
+	  // variableWidth: true
+
+	  responsive: [
+	  {
+      breakpoint: 768,
+      settings: {
+    	  slidesToShow: 1,
+			  centerMode: true,
+			  variableWidth: true
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3, 
+      }
+    },
+    {
+      breakpoint: 10000,
+      settings: {
+    	  slidesToShow: 4,
+      }
+    },
+    
+  ]
+
+  });
+
+  
 });
+
