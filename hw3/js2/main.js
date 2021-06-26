2a530cc4a0d9ac14ccd7c217ff92026c862b3975
Vladyslav Polushyn <polexodus@gmail.com>
2021-06-26 12:38:02 +0300

    Completed JS task 2

--- hw3/js2/main.js
+++ hw3/js2/main.js
@@ -17,20 +17,112 @@ function rollDominoes() {
 
 	rollBtn.setAttribute('disabled', 'disabled');
 
+	let rollNum = false;
+
+	const numCount = {
+		1: 0,
+		2: 0,
+		3: 0,
+		4: 0,
+		5: 0,
+		6: 0
+	};
+
+	for (let i = 0; i < numOfDominoes; i++) {
+
+		numCount[tops[i]] += 1;
+		// Check unnecessary increment if double
+		if (tops[i] !== bottoms[i]) {
+			numCount[bottoms[i]] += 1;
+		}
+		
+	}
+
+	for (let key in numCount) {
+
+		if (numCount[key] === numOfDominoes) {
+			rollNum = +key;
+		}
+
+	}
+
+	if (!rollNum) {
+		outputText.innerHTML = '-1';
+	} else {
+
+		let topCount = 0;
+		let bottomCount = 0;
+
+		for (let i = 0; i < numOfDominoes; i++) {
+
+			if (tops[i] !== bottoms[i]) {
+
+				if (tops[i] === rollNum) {
+					topCount++;
+				} else if (bottoms[i] === rollNum) {
+					bottomCount++;
+				}
+
+			}
+
+		}
+
+		for (let i = 0; i < numOfDominoes; i++) {
+
+			if (topCount > bottomCount) {
+
+				if (bottoms[i] === rollNum) {
+					let temp = tops[i];
+					tops[i] = bottoms[i];
+					bottoms[i] = temp;
+				}
+
+				outputText.innerHTML = bottomCount;
+
+			} else {
+
+				if (tops[i] === rollNum) {
+					let temp = bottoms[i];
+					bottoms[i] = tops[i];
+					tops[i] = temp;
+				}
+
+				outputText.innerHTML = topCount;
+
+			}
+
+		}
+
+		dotsRender();
+
+	}
+
+}
+
+function dotsRender() {
+	for (let i = 0; i < numOfDominoes; i++) {
+		topDominoesBlocks[i].className = `domino dot-${tops[i]}`;
+		bottomDominoesBlocks[i].className = `domino dot-${bottoms[i]}`;
+	}
 }
 
 function generateDominoes() {
 	// Clear the previous genaration
 	resetDominoes();
+	outputText.innerHTML = '';
 
-	// Generat the new Array of Dominoes and Render it
+	// tops = [2,1,2,4,2,2];
+	// bottoms = [5,2,6,2,3,2];
+	// dotsRender();
+
+	// Generating the new Array of Dominoes and Render it
+	// You can comment random Dominoes rendering and set the values above
 
 	for (let i = 0; i < numOfDominoes; i++) {
 		tops.push(randNum(1, 6));
 		bottoms.push(randNum(1, 6));
 
-		topDominoesBlocks[i].classList.add(`dot-${tops[i]}`);
-		bottomDominoesBlocks[i].classList.add(`dot-${bottoms[i]}`);
+		dotsRender();
 	}
 
 	rollBtn.removeAttribute('disabled');
