4c16d80a90d359e8ffa22ab4d15066bbaa82bcaa
Vladyslav Polushyn <polexodus@gmail.com>
2021-07-01 14:38:55 +0300

    Completed js task 4

---
+++ hw3/js4/main.js
@@ -0,0 +1,65 @@
+
+getFriendlyNumbers(200, 10000);
+
+function getFriendlyNumbers(start, end) {
+
+	if ( start > end || start < 0 || end < 0 || typeof start !== 'number' || typeof end !== 'number' ) {
+		return false;
+	}
+
+	let nums = [];
+	let sumOfDivisors = [];
+	let result = [];
+	let existNums =[];
+
+	for (let i = start; i <= end; i++) {
+
+		if (i === 1) {
+			continue;
+		}
+
+		nums.push(i);
+		sumOfDivisors.push( getSum(getDivisors(i)) );
+	}
+
+	for (let i = 0; i < nums.length; i++) {
+
+		if ( (sumOfDivisors.indexOf(nums[i]) >= 0) && (nums[sumOfDivisors.indexOf(nums[i])] === sumOfDivisors[i]) && (nums[i] !== sumOfDivisors[i]) ) {
+
+			if (existNums.includes(nums[i]) || existNums.includes(nums[sumOfDivisors.indexOf(nums[i])])) {
+				continue;
+			}
+			
+			result.push( [ nums[i], nums[sumOfDivisors.indexOf(nums[i])] ] );
+			existNums.push(nums[i], nums[sumOfDivisors.indexOf(nums[i])]);
+
+		}
+
+	}
+
+	console.log(result);
+	return result;
+
+}
+
+function getDivisors(num) {
+	let result = [];
+
+	for (let i = 1; i < num; i++) {
+		if (num % i === 0) {
+			result.push(i);
+		}
+	}
+
+	return result;
+
+}
+
+function getSum(arr) {
+
+	if (arr.length !== 0) {
+		return arr.reduce( (accum, current) => accum + current );
+	}
+	
+}
+
